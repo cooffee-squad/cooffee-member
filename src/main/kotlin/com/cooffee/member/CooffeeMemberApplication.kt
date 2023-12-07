569bb7068f0c0acd948c9f13c2fa1b7ace424f1f
kadun1 <kadunjj@gmail.com>
2023-12-07 23:03:44 +0900

    chore: init project

---
+++ src/main/kotlin/com/cooffee/member/CooffeeMemberApplication.kt
@@ -0,0 +1,11 @@
+package com.cooffee.member
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CooffeeMemberApplication
+
+fun main(args: Array<String>) {
+    runApplication<CooffeeMemberApplication>(*args)
+}
---
+++ src/test/kotlin/com/cooffee/member/CooffeeMemberApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cooffee.member
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CooffeeMemberApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
